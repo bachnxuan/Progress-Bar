8b38d03b1de3c169292f180f439a7c503e2e9664
bachnxuan <76820483+bachnxuan@users.noreply.github.com>
2021-08-18 21:06:59 +0700

    Add files via upload

---
+++ Progressbar.py
@@ -0,0 +1,27 @@
+import sys
+import time
+import os
+def progressBar():
+	animation     = ["[----------]","[>--------]","[>>--------]", "[>>>-------]", "[>>>>------]", "[>>>>>-----]", "[>>>>>>----]", "[>>>>>>>---]", "[>>>>>>>>--]", "[>>>>>>>>>-]"]
+	progress_anim = 0
+	save_anim     = animation[progress_anim % len(animation)]
+	percent       = 0
+	while True:
+		for i in range(10):
+			percent += 1
+			sys.stdout.write(f"\rDownload ...  " + save_anim + f" {percent}%")
+			sys.stdout.flush()
+			time.sleep(0.075)
+		progress_anim += 1
+		save_anim = animation[progress_anim % len(animation)]
+		if percent == 100:
+			sys.stdout.write("\r[+] Request completed... [>>>>>>>>>>] 100%")
+			break
+
+while True:
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print("")
+	print("")
+	sys.stdout.write(f"\r[+] Sending request...   [----------] 0% {progressBar()}")
+	sys.stdout.flush()
+	break
